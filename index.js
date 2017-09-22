c54810d610b661dcf1f66ee12b6bc4f2a4de8be7
kanapopom <k.9vttx.s2@gmail.com>
2017-09-22 21:27:35 +0900

    index.js change

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let restify = require('restify');
 let line = require("@line/bot-sdk");
+let bot = new line.Client(LINE_CONFIG);
+
 
 const LINE_CONFIG = {
     channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
@@ -15,16 +17,8 @@ server.use(line.middleware(LINE_CONFIG));
 
 server.post('/webhook', (req, res, next) => {
     res.send(200);
-    console.log(req.body);
-});
-
-let bot = new line.Client(LINE_CONFIG);
-
-server.post('/webhook', (req, res, next) => {
-    res.send(200);
 
     let events_processed = [];
-
 
     req.body.events.map((event) => {
 
