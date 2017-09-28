3acb2c0b576a3daca551c9c79da9635395ad70c7
kanapopom <k.9vttx.s2@gmail.com>
2017-09-28 23:07:49 +0900

    api.ai導入

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let restify = require("restify");
 let line = require("@line/bot-sdk");
+let apiai = require("apiai-promisified");
 
 const LINE_CONFIG = {
     channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
     channelSecret: process.env.LINE_CHANNEL_SECRET
 };
+const APIAI_CLIENT_ACCESS_TOKEN = process.env.APIAI_CLIENT_ACCESS_TOKEN;
+
+
 let server = restify.createServer();
 server.listen(process.env.PORT || 3000, function() {
     console.log("Node is running.");
@@ -13,28 +17,56 @@ server.listen(process.env.PORT || 3000, function() {
 server.use(line.middleware(LINE_CONFIG));
 
 let bot = new line.Client(LINE_CONFIG);
+let nlp = new apiai(APIAI_CLIENT_ACCESS_TOKEN, {language: "ja"});
 
 server.post('/webhook', (req, res, next) => {
+
     res.send(200);
 
     let events_processed = [];
 
     req.body.events.map((event) => {
 
-            if (event.type == "message" || event.message.type == "text"){
-               if (event.message.text == "こんにちは"){
+        if (event.type == "message" || event.message.type == "text"){
+            events_processed.push(
 
-                    events_processed.push(bot.replyMessage(event.replyToken, {
-                        type: "text",
-                        text: "これはこれは"
-                    }));
-                }
-            }
-        });
+                nlp.textRequest(event.message.text, {sessionId: event.source.userId}).then(
+                    (response) => {
+                        console.log(`The action is ${response.result.action}.`);
+                        let reply_message;
 
-        Promise.all(events_processed).then(
-            (response) => {
-                console.log(`${response.length} events processed.`);
-            }
-        );
+                        if (response.result.action == "diet-encourage"){
+
+                            if (response.result.parameters.change === ""){
+                                reply_message = `お前、最近頑張ってるみたいだな`;
+                            } else {
+                                if (response.result.parameters.parts === ""){
+                                    reply_message = `お前、${response.result.parameters.parts}が${response.result.parameters.change}`;
+                                }
+                            }
+                        } else if (response.result.action == "input.unknown"){
+                            // もしactionがinput.unknownだった場合（意図が判定できなかった場合）はプリセットのメッセージをセット。
+                            reply_message = response.result.fulfillment.speech;
+                        }
+
+                        // 返信。
+                        return bot.replyMessage(event.replyToken, {
+                            type: "text",
+                            text: reply_message
+                        });
+                    }
+                )
+            );
+        }
     });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} events processed.`);
+        },
+        (error) => {
+            console.log(error);
+        }
+    );
+});
