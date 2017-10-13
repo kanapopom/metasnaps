e375833c57b1a95e776bc0069acff50ce1e5da9b
kanapopom <k.9vttx.s2@gmail.com>
2017-10-13 23:29:47 +0900

    imanani

--- index.js
+++ index.js
@@ -51,6 +51,9 @@ server.post('/webhook', (req, res, next) => {
                                         else if (response.result.action == "nagu"){
                                                 reply_message = `${response.result.fulfillment.speech}`;
                                             }
+                                            else if (response.result.action == "imanani"){
+                                                    reply_message = `${response.result.fulfillment.speech}`;
+                                                }
                         else if (response.result.action == "input.unknown"){
                             reply_message = response.result.fulfillment.speech;
                         }
